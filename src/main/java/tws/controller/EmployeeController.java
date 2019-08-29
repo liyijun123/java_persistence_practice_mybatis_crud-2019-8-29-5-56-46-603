2411e708b08ac1f5c2ba76307a4e5c7ddb34a93c
“liyijun123 <2510243137@qq.com>
2019-08-29 16:52:10 +0800

    finish

--- src/main/java/tws/controller/EmployeeController.java
+++ src/main/java/tws/controller/EmployeeController.java
@@ -2,11 +2,17 @@ package tws.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.fasterxml.jackson.annotation.JsonTypeInfo.Id;
+
 import tws.entity.Employee;
 import tws.repository.EmployeeMapper;
 
@@ -21,9 +27,39 @@ public class EmployeeController {
     @Autowired
     private EmployeeMapper employeeMapper;
 
+
     @GetMapping("")
     public ResponseEntity<List<Employee>> getAll() {
         return ResponseEntity.ok(employeeMapper.selectAll());
+    }
+    
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Employee> updateOne(@PathVariable String id,@RequestBody Employee employee) {
+    	employeeMapper.updateOne(id,employee);
+    	    return ResponseEntity.ok(employee);
+    }
+    
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Employee> getOne(@PathVariable String id) {
+    	Employee employee = employeeMapper.selectOne(id);
+        return ResponseEntity.ok(employee);
+    }
+    
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Employee> deleteOne(@PathVariable String id) {
+    	Employee employee = employeeMapper.deleteOne(id);
+        return ResponseEntity.ok(employee);
+    }
+    
+
+    @PostMapping()
+    public ResponseEntity<Employee> addEmployee(@RequestBody Employee employee) {
+        String id = UUID.randomUUID().toString();
+        employee.setAge(id);
+        employeeMapper.addEmployee(employee);
+        return ResponseEntity.created(URI.create("/employees"+id)).build();
     }
 
 }
--- src/main/java/tws/repository/EmployeeMapper.java
+++ src/main/java/tws/repository/EmployeeMapper.java
@@ -1,6 +1,8 @@
 package tws.repository;
 
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
 import tws.entity.Employee;
 
 import java.util.List;
@@ -8,4 +10,14 @@ import java.util.List;
 @Mapper
 public interface EmployeeMapper {
     List<Employee> selectAll();
+   
+    void insert(@Param("employee")Employee employee);
+	
+    void addEmployee(@Param("employee")Employee employee);
+	
+    Employee selectOne(@Param("id")String id) ;
+	
+    void updateOne(@Param("id")String id,@Param("employee")Employee employee);
+
+	Employee deleteOne(String id);
 }
